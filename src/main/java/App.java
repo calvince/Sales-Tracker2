5edae6b8bbb21e28ad513a80b12d7f0a09a0049d
Annette Odingo <odingoannette@gmail.com>
2019-07-12 08:36:04 +0300

    Add details to App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,24 +4,37 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
+import dao.Sql2oStoreDao;
+import models.Store;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import exceptions.ApiException;
+
 
 import static spark.Spark.*;
 
+
+
+
 public class App {
+//
+//    static int getHerokuAssignedPort() {
+//        ProcessBuilder processBuilder = new ProcessBuilder();
+//        if (processBuilder.environment().get("PORT") != null) {
+//            return Integer.parseInt(processBuilder.environment().get("PORT"));
+//        }
+//        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+//    }
     public static void main(String[]args){
-        ProcessBuilder process =new ProcessBuilder();
-        Integer port;
-
-        if(process.environment().get("PORT")!= null){
-            port = Integer.parseInt(process.environment().get("PORT"));
-
-        }
-        else{
-            port =4567;
-        }
-        port(port);
-
         staticFileLocation("/public");
+//        port(getHerokuAssignedPort());
+
+        Sql2oStoreDao storeDao;
+        Connection conn;
+        String connectionString = "jdbc:postgresql://localhost:5432/salestracker";
+        Sql2o sql2o = new Sql2o(connectionString, "xkddftxsfraqws", "5f5d3259bc8ebb3621205bc08f55d9ecf0df2c958d5f2aef0253555a9c22f619");
+        storeDao = new Sql2oStoreDao(sql2o);
+
 
         get("/stores", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
@@ -36,7 +49,7 @@ public class App {
             int itemId = Integer.parseInt(request.queryParams("itemId"));
             Store newStore = new Store(storeName, location, storeCode, itemId );
             storeDao.add(newStore);
-            model.put("stores", storeDao.getAll());
+            model.put("stores", storeDao.getAllStore());
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
